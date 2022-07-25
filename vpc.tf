7d97e2a896969eb9f8cf1e29c02c2296586de501
root <root@ip-10-0-5-156.ec2.internal>
2022-07-25 17:31:01 +0000

    changing the volume size

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAQVBOV3PZ7SDHP5VJ"
+  secret_key = "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"
+}
+
+
+resource "aws_ebs_volume" "my_ebs" {
+  availability_zone = "us-west-2a"
+  size              = 10
+
+  tags = {
+    Name = "myebsvol"
+  }
+}
