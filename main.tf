db76d49f0bd2173d424d83cf059abba34171fb70
root <root@ip-10-0-5-156.ec2.internal>
2022-07-25 17:20:29 +0000

    changing the region

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region     = "us-west-1"
+  region     = "us-west-2"
   access_key = "AKIAQVBOV3PZ7SDHP5VJ"
   secret_key = "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"
 }
 
 
 resource "aws_ebs_volume" "my_ebs" {
-  availability_zone = "us-west-1a"
+  availability_zone = "us-west-2a"
   size              = 5
 
   tags = {
